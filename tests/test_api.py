8c2d510912a920433f465010321e3eeb88a129a0
leungyukshing <jacky14.liang@gmail.com>
2020-01-01 17:11:32 +0800

    fix: fix unit test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
 # coding=utf-8
 import pytest
+from app import app
+import json
 
 def test_app():
     assert 5 == 5
-'''
-@pytest.fixture()
+
+
+@pytest.fixture
 def client():
     test_client = app.test_client()
-    yield  test_client
+    yield test_client
+
 
 def test_search(client):
-    rv = client.post('htpps://localhost:5000/search')
-'''
+    rv = client.post('http://127.0.0.1:5000/search')
+    resp = json.loads(rv.data)
+    assert 'ok' == resp["message"]
+
 
 if __name__ == "__main__":
     pytest.main()
