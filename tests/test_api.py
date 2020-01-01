f551d0e5594f2dabf11a5d29d7eda0cf9f9eff9f
leungyukshing <jacky14.liang@gmail.com>
2020-01-01 23:11:08 +0800

    test: should failed

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,4 +14,4 @@ def test_search(client):
     client = app.test_client()
     rv = client.post('http://localhost:5000/search')
     resp = json.loads(rv.data)
-    assert 'ok' == resp['message']
+    assert 'okkk' == resp['message']
