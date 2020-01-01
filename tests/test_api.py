773c0c22081869fc4f7275c947b1f98f5c159367
leungyukshing <jacky14.liang@gmail.com>
2020-01-01 22:54:40 +0800

    fix: fix pytest client

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,9 +2,14 @@
 # coding=utf-8
 from app import app
 import json
+import pytest
 
+@pytest.fixture
+def client():
+    test_client = app.test_client()
+    yield test_client
 
-def test_search():
+def test_search(client):
     print('In test_search')
     client = app.test_client()
     rv = client.post('http://localhost:5000/search')
