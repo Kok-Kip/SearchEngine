5eef70eb38b7682846c9b721cf4f9b003da5c21e
leungyukshing <jacky14.liang@gmail.com>
2020-02-06 16:29:05 +0800

    doc: add comments

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from app import app
 
 
+# Run on Remote Server with the following cmd:
+# nohup python3 server.py > debug.log 2>&1
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
